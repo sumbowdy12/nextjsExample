a123ac9370ec15c070478c071dea054936a8a413
Boris Le <brsl20@dordt.edu>
2024-05-26 20:19:05 +1000

    dwda
    dwad

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
-
+/*dwada*/
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
